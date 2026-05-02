c1f8e7bb64177a1409d70ba487827e0bce0106de
scraane <stephan@nexed.tech>
2026-05-02 20:49:55 +0800

    Update Nextcloud OIDC configuration to enforce group-based login restrictions in SshInstaller

--- app/Services/SshInstaller.php
+++ app/Services/SshInstaller.php
@@ -447,7 +447,8 @@ docker exec -u www-data nextcloud-aio-nextcloud php occ user_oidc:provider Keycl
     --send-id-token-hint=1 \
     --mapping-uid=email \
     --unique-uid=0 \
-    --auto-provision=0 < /dev/null
+    --group-restrict-login-to-whitelist=1 \
+    --group-whitelist-regex='.*' < /dev/null
 echo "  OpenID Connect configured."
 BASH);
     }
